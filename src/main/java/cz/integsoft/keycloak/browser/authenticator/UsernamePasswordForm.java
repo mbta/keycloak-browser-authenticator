2309d1894eef67dfcfd7d6429cd8b280b2a17a3c
Tomas Kozany <tomas.kozany@integsoft.cz>
2023-04-26 11:46:51 +0200

    Prihlasovani pomoci Bcrypt hesla z atributu uzivatele.

--- src/main/java/cz/integsoft/keycloak/browser/authenticator/UsernamePasswordForm.java
+++ src/main/java/cz/integsoft/keycloak/browser/authenticator/UsernamePasswordForm.java
@@ -1,5 +1,8 @@
 package cz.integsoft.keycloak.browser.authenticator;
 
+import static org.keycloak.services.validation.Validation.FIELD_PASSWORD;
+import static org.keycloak.services.validation.Validation.FIELD_USERNAME;
+
 import java.util.Locale;
 import java.util.UUID;
 
@@ -12,14 +15,24 @@ import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.authenticators.browser.AbstractUsernameFormAuthenticator;
+import org.keycloak.events.Details;
+import org.keycloak.events.Errors;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ModelDuplicateException;
+import org.keycloak.models.ModelException;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserCredentialModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.FormMessage;
+import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.services.ServicesLogger;
 import org.keycloak.services.managers.AuthenticationManager;
+import org.keycloak.services.messages.Messages;
 import org.keycloak.services.validation.Validation;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 /**
  * Username password browser authenticator.
@@ -49,7 +62,7 @@ public class UsernamePasswordForm extends AbstractUsernameFormAuthenticator impl
 			context.failureChallenge(AuthenticationFlowError.INVALID_USER, challenge);
 			return;
 		}
-		if (!validateForm(context, formData)) {
+		if (!validatePassword(context, formData)) {
 			return;
 		}
 		// check existence of mbta_uuid
@@ -61,8 +74,111 @@ public class UsernamePasswordForm extends AbstractUsernameFormAuthenticator impl
 		context.success();
 	}
 
-	protected boolean validateForm(final AuthenticationFlowContext context, final MultivaluedMap<String, String> formData) {
-		return validateUserAndPassword(context, formData);
+	private boolean validatePassword(final AuthenticationFlowContext context, final MultivaluedMap<String, String> inputData) {
+		context.clearUser();
+		final UserModel user = getUser(context, inputData);
+		// if the user has a "password" attribute, validate it. Remove "password" from attributes if valid.
+		if (user != null) {
+			logger.debugf("Has user %s password configured: %s", user.getUsername(), context.getSession().userCredentialManager().isConfiguredFor(context.getRealm(), user, CredentialRepresentation.PASSWORD));
+			if (context.getSession().userCredentialManager().isConfiguredFor(context.getRealm(), user, CredentialRepresentation.PASSWORD)) {
+				if (user.getFirstAttribute(CredentialRepresentation.PASSWORD) != null) {
+					user.removeAttribute(CredentialRepresentation.PASSWORD);
+				}
+				return validateUserAndPassword(context, inputData) && validateUser(context, user, inputData);
+			}
+			// if user has old hash type, we have to change it
+			String passwordHashAttribute = user.getFirstAttribute(CredentialRepresentation.PASSWORD);
+			if (passwordHashAttribute == null) {
+				return badPasswordHandler(context, user, true, false);
+			}
+			if (passwordHashAttribute.startsWith("$2y$")) {
+				passwordHashAttribute = passwordHashAttribute.replace("$2y$", "$2a$");
+			}
+			final String password = inputData.getFirst(CredentialRepresentation.PASSWORD);
+			final boolean valid = BCrypt.checkpw(password, passwordHashAttribute);
+			logger.debugf("User %s attribut password valid: %s", user.getUsername(), valid);
+			if (!valid) {
+				return badPasswordHandler(context, user, true, false);
+			}
+			context.setUser(user);
+			// store the password in Keycloak
+			// we catch an exception if the password does not meet the password policy
+			try {
+				context.getSession().userCredentialManager().updateCredential(context.getRealm(), user, UserCredentialModel.password(password));
+				user.removeAttribute(CredentialRepresentation.PASSWORD);
+			} catch (final ModelException e) {
+				logger.infof("Password policy - unable to set password as Keycloak password of user %s.", user.getUsername());
+			}
+			return valid;
+		}
+		return false;
+	}
+
+	// Set up AuthenticationFlowContext error.
+	private boolean badPasswordHandler(final AuthenticationFlowContext context, final UserModel user, final boolean clearUser, final boolean isEmptyPassword) {
+		context.getEvent().user(user);
+		context.getEvent().error(Errors.INVALID_USER_CREDENTIALS);
+		final Response challengeResponse = challenge(context, getDefaultChallengeMessage(context), FIELD_PASSWORD);
+		if (isEmptyPassword) {
+			context.forceChallenge(challengeResponse);
+		} else {
+			context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challengeResponse);
+		}
+
+		if (clearUser) {
+			context.clearUser();
+		}
+		return false;
+	}
+
+	private UserModel getUser(final AuthenticationFlowContext context, final MultivaluedMap<String, String> inputData) {
+		String username = inputData.getFirst(AuthenticationManager.FORM_USERNAME);
+		if (username == null) {
+			context.getEvent().error(Errors.USER_NOT_FOUND);
+			final Response challengeResponse = challenge(context, getDefaultChallengeMessage(context), FIELD_USERNAME);
+			context.failureChallenge(AuthenticationFlowError.INVALID_USER, challengeResponse);
+			return null;
+		}
+
+		// remove leading and trailing whitespace
+		username = username.trim();
+
+		context.getEvent().detail(Details.USERNAME, username);
+		context.getAuthenticationSession().setAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME, username);
+
+		UserModel user = null;
+		try {
+			user = KeycloakModelUtils.findUserByNameOrEmail(context.getSession(), context.getRealm(), username);
+		} catch (final ModelDuplicateException mde) {
+			ServicesLogger.LOGGER.modelDuplicateException(mde);
+
+			// Could happen during federation import
+			if (mde.getDuplicateFieldName() != null && mde.getDuplicateFieldName().equals(UserModel.EMAIL)) {
+				setDuplicateUserChallenge(context, Errors.EMAIL_IN_USE, Messages.EMAIL_EXISTS, AuthenticationFlowError.INVALID_USER);
+			} else {
+				setDuplicateUserChallenge(context, Errors.USERNAME_IN_USE, Messages.USERNAME_EXISTS, AuthenticationFlowError.INVALID_USER);
+			}
+			return user;
+		}
+
+		testInvalidUser(context, user);
+		return user;
+	}
+
+	private boolean validateUser(final AuthenticationFlowContext context, final UserModel user, final MultivaluedMap<String, String> inputData) {
+		if (!enabledUser(context, user)) {
+			return false;
+		}
+		final String rememberMe = inputData.getFirst("rememberMe");
+		final boolean remember = rememberMe != null && rememberMe.equalsIgnoreCase("on");
+		if (remember) {
+			context.getAuthenticationSession().setAuthNote(Details.REMEMBER_ME, "true");
+			context.getEvent().detail(Details.REMEMBER_ME, "true");
+		} else {
+			context.getAuthenticationSession().removeAuthNote(Details.REMEMBER_ME);
+		}
+		context.setUser(user);
+		return true;
 	}
 
 	@Override
